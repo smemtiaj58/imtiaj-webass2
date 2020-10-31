fb5197d29c0dd1f3c43c74f28f8853177f0d1909
smemtiaj58 <73761855+smemtiaj58@users.noreply.github.com>
2020-10-31 19:39:19 -0400

    Update airbnb.js

--- airbnb.js
+++ airbnb.js
@@ -1,22 +1,10 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+var express = require('express');
+var app = express();
+var path = require('path');
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.use(express.static(__dirname + "/views"));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-router.get('/room',function(req,res){
-  res.sendFile(path.join(__dirname+'/roompage.html'));
-});
-
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
-
-
-
+var HTTP_PORT = process.env.PORT;
+ app.listen(HTTP_PORT);
