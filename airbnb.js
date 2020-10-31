c5e150dc6e11c9cb0bb77b4bc5302f37bfa28807
smemtiaj58 <73761855+smemtiaj58@users.noreply.github.com>
2020-10-31 19:40:38 -0400

    Update airbnb.js

--- airbnb.js
+++ airbnb.js
@@ -1,10 +1,56 @@
-var express = require('express');
+
 var app = express();
-var path = require('path');
+var bodyParser = require('body-parser');
+const Handlebars = require("handlebars");
+const { body, validationResult } = require('express-validator');
+
+var HTTP_PORT = process.env.PORT || 3000;
+
 
 app.use(express.static(__dirname + "/views"));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/views/index.html");
+   
 });
-var HTTP_PORT = process.env.PORT;
- app.listen(HTTP_PORT);
+
+app.get("/room", function(req,res){
+    res.sendfile(__dirname + "/views/roompage.html");
+    
+});
+
+
+
+app.post('/action_page', [
+  
+  body('username').isEmail(),
+  body('password').isLength({ min: 5 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(user => res.json(user));
+});
+
+app.post('/form-reg', [
+  
+    body('username').isEmail(),
+    body('password').isLength({ min: 5 })
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  });
+
+
+
+
+
+  app.listen(HTTP_PORT);
