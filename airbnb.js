14ce7646bacde45aa9a14d8ef2b3a9024c3535d8
smemtiaj58 <73761855+smemtiaj58@users.noreply.github.com>
2020-11-11 18:40:47 -0500

    Update airbnb.js

--- airbnb.js
+++ airbnb.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -5,8 +6,6 @@ const Handlebars = require("handlebars");
 const { body, validationResult } = require('express-validator');
 
 var HTTP_PORT = process.env.PORT || 3000;
-
-
 
 
 app.use(express.static(__dirname + "/Views"));
@@ -21,6 +20,35 @@ app.get("/room", function(req,res){
     res.sendfile(__dirname + "/Views/roompage.html");
     
 });
+
+
+
+app.post('/action_page', [
+  
+  body('username').isEmail(),
+  body('password').isLength({ min: 5 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(user => res.json(user));
+});
+
+app.post('/form-reg', [
+  
+    body('username').isEmail(),
+    body('password').isLength({ min: 5 })
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  });
 
 
   app.listen(HTTP_PORT,onHttpStart);
