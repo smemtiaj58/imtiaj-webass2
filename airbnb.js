b1bfa365937dfff0700d21c8124ebd5160a0f68b
smemtiaj58 <73761855+smemtiaj58@users.noreply.github.com>
2020-11-11 18:33:36 -0500

    Update airbnb.js

--- airbnb.js
+++ airbnb.js
@@ -7,18 +7,20 @@ const { body, validationResult } = require('express-validator');
 var HTTP_PORT = process.env.PORT || 3000;
 
 
-app.use(express.static("./views"));
+
+app.use(express.static(__dirname + "/views"));
 
 
 app.get("/", function(req,res){
-    res.sendFile("./views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
    
 });
 
 app.get("/room", function(req,res){
-    res.sendfile("./views/roompage.html");
+    res.sendfile(__dirname + "/views/roompage.html");
     
 });
+
 
   app.listen(HTTP_PORT,onHttpStart);
   
