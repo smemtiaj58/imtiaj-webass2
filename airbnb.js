f5a99c8b948e0d419b32fb4a953674d2c24b426b
smemtiaj58 <73761855+smemtiaj58@users.noreply.github.com>
2020-11-11 18:27:19 -0500

    Update airbnb.js

--- airbnb.js
+++ airbnb.js
@@ -20,36 +20,6 @@ app.get("/room", function(req,res){
     
 });
 
-
-
-app.post('/action_page', [
-  
-  body('username').isEmail(),
-  body('password').isLength({ min: 5 })
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  User.create({
-    username: req.body.username,
-    password: req.body.password
-  }).then(user => res.json(user));
-});
-
-app.post('/form-reg', [
-  
-    body('username').isEmail(),
-    body('password').isLength({ min: 5 })
-  ], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-  });
-
-
   app.listen(HTTP_PORT,onHttpStart);
   
   function onHttpStart() {
